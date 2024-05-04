6543bde7e3274a8f4724b8685eb9234c245fa99e
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-05-05 00:45:07 +0200

    last functionnal state before CRUD

--- quai_antique_back/src/Controller/RestaurantController.php
+++ quai_antique_back/src/Controller/RestaurantController.php
@@ -3,30 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('api/restaurant', name: 'app_api_restaurant_')]
 class RestaurantController extends AbstractController
-
 {
-    #[Route(name:'new', methods:'POST')]
-    public function new(): Response
+    #[Route('/restaurant', name: 'app_restaurant')]
+    public function index(): JsonResponse
     {
-    }
-
-    #[Route('/', name:'show', methods:'GET')]
-    public function show(): Response
-    {
-    }
-
-    #[Route('/', name:'edit', methods:'PUT')]
-    public function edit(): Response
-    {
-    }
-
-    #[Route('/', name:'delete', methods:'DELETE')]
-    public function delete(): Response
-    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/RestaurantController.php',
+        ]);
     }
 }
