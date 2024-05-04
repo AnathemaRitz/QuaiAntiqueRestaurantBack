c0b6572d9acecb35cf50c3b41d79d154ec68914d
AnathemaRitz <149684298+AnathemaRitz@users.noreply.github.com>
2024-05-04 23:46:35 +0200

    premier implément CRUD

--- quai_antique_back/src/Controller/RestaurantController.php
+++ quai_antique_back/src/Controller/RestaurantController.php
@@ -3,17 +3,30 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('api/restaurant', name: 'app_api_restaurant_')]
 class RestaurantController extends AbstractController
+
 {
-    #[Route('/restaurant', name: 'app_restaurant')]
-    public function index(): JsonResponse
+    #[Route(name:'new', methods:'POST')]
+    public function new(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/RestaurantController.php',
-        ]);
+    }
+
+    #[Route('/', name:'show', methods:'GET')]
+    public function show(): Response
+    {
+    }
+
+    #[Route('/', name:'edit', methods:'PUT')]
+    public function edit(): Response
+    {
+    }
+
+    #[Route('/', name:'delete', methods:'DELETE')]
+    public function delete(): Response
+    {
     }
 }
